77659eb0db64376116064e0f5a3c6763c7d7fd79
wangadd <1276321779@qq.com>
2019-03-12 10:54:40 +0000

    debug

--- test.php
+++ test.php
@@ -4,10 +4,10 @@ $server=[
 	        '192.168.80.112'
 	];
 foreach ($server as $k=>$v){
-	    $cmd='ssh '.$v.'" cd /www/test ; git pull"';
-	        echo $cmd."<br>";
-	        $res=shell_exec($cmd);
-		    echo $res."</hr>";
-}
-
-?>
+	    $cmd='ssh '.$v.'" cd /www/test ; git clone git@github.com:wangadd/test.git"';
+	    //    echo $cmd."<br>";
+	           $res=shell_exec($cmd);
+	               echo $res."</hr>";
+	               }
+	   
+	   ?>
