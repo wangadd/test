776144742a5a49522275d501cb2fb5a91666f4e7
wangadd <1276321779@qq.com>
2019-03-13 00:42:55 +0000

    bug

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 <?php
+//test1111
 $server=[
 	    '192.168.80.111',
 	        '192.168.80.112'
 	];
 foreach ($server as $k=>$v){
-	    $cmd='ssh '.$v.'" cd /www/test ; git clone git@github.com:wangadd/test.git"';
+	    $cmd='ssh '.$v.'" cd /www/test && git clone git@github.com:wangadd/test.git"';
 	    //    echo $cmd."<br>";
 	           $res=shell_exec($cmd);
 	               echo $res."</hr>";
